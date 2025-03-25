95c9eb56fca31a1fd2ea17f8a5655bdbb2e76d3b
pereiramelissa <50351734+pereiramelissa@users.noreply.github.com>
2025-03-25 15:05:52 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Demo{
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 25;
+        arr[2] = 40;
+        System.out.println(Arrays.toString(arr));
+    }
+}
