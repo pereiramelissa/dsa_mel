20ffa8933a50f96e175c684c676275dfae7565c0
pereiramelissa <50351734+pereiramelissa@users.noreply.github.com>
2025-03-25 15:31:55 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class EvenorOdd {
+    public static void main(String[] args) {
+        int num = 3;
+        if (num % 2 == 0) {
+            System.out.println("Entered number is Even");
+        } else {
+            System.out.println("Entered number is Odd");
+        }
+    }
+}
